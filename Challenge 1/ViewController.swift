d3bce603210c35b9cf1e0f98ecb86a2b71e3733d
marcboanas <marcjohnboanas@gmail.com>
2014-10-05 13:22:55 +0100

    challenge completed

--- Challenge 1/ViewController.swift
+++ Challenge 1/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func buttonPressed(sender: UIButton) {
+        
+        self.nameLabel.hidden = false
+        self.nameLabel.text = self.textField.text
+        self.nameLabel.textColor = UIColor.blueColor()
+        self.textField.text = ""
+        self.textField.resignFirstResponder()
+    }
 }
 
